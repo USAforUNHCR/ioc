b72353208dc12b6ca8e5ffcefd99277ed545e981
Chris Wallwork <chris@unrefgugees.org>
2016-03-02 18:13:52 -0500

    added basic page and form

---
+++ _assets/javascripts/home.js
@@ -0,0 +1,48 @@
+var gw = new Groundwork ({
+    'api_url': 'https://api.thegroundwork.com',
+    'oauth_client_id': 'pub-un-test.olympics--Gp9kL6zhJ20.teMAuXMAvmtMjh.9KLXwNbiumd.dMsxFGwwFYkUqqjb2UTtRumvrf_9faJXkCV8faQV9fhTGXg'
+  });
+
+function splitNames(name){
+  var nameArr = name.split(" ");
+  var givenName = nameArr[0];
+  var familyName = "";
+  for (var i = 1; i< nameArr.length; i++) {
+    familyName = familyName + " " + nameArr[i];
+  }
+  return {
+    familyName: familyName,
+    givenName: givenName
+  };
+}
+
+$(document).ready(function(){
+  formListener();
+});
+
+function formListener() {
+  $('#petition').submit(function(event) {
+    event.preventDefault()
+    var form = $('#petition');
+    var data = splitNames(form.find('[name="NAME"]').val());
+    data.email = form.find('[name="EMAIL"]').val();
+    data.postalCode = form.find('[name="ZIP"]').val();
+    data.source = "olympic signup";
+    sendData(data);
+    form.find('input').val('');
+    form.find('[type="submit"]').prop('value','Thanks!');
+    form.find('[type="submit"]').prop('disabled', true);
+  });
+}
+
+function sendData(data) {
+  data.tags || (data.tags = {});
+  data.tags.send_email = 0;
+  gw.supporters.create(data)
+  .then(function(res){
+    console.log(res);
+  })
+  .catch(function(res){
+    console.log(res);
+  });
+};
