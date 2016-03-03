7e60fe31311eaac21f82097ab1c0103e1a2a664a
Chris Wallwork <chris@unrefgugees.org>
2016-03-03 11:33:28 -0500

    added confirmation page, social links and query string to source

--- _assets/javascripts/home.js
+++ _assets/javascripts/home.js
@@ -27,7 +27,7 @@ function formListener() {
     var data = splitNames(form.find('[name="NAME"]').val());
     data.email = form.find('[name="EMAIL"]').val();
     data.postalCode = form.find('[name="ZIP"]').val();
-    data.source = "olympic signup";
+    data.source = querystring.parse().src;
     sendData(data);
     form.find('input').val('');
     form.find('[type="submit"]').prop('value','Thanks!');
@@ -41,6 +41,7 @@ function sendData(data) {
   gw.supporters.create(data)
   .then(function(res){
     console.log(res);
+    // window.location.href = '../confirmation.html';
   })
   .catch(function(res){
     console.log(res);
